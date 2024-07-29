74084b93a65f591759801db7494cc3eb376164e7
ricpaula <cadodepaulasilva159@gmail.com>
2024-07-29 17:36:26 -0300

    Retornando alterações para a versão anterior.

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Telaflix 2 | Busque todas a informações de filmes e séries.</title>
+  <title>Telaflix | Busque todas a informações de filmes e séries.</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <style>
@@ -30,7 +30,7 @@
 <body>
   <div class="container d-flex flex-column mb-3 text-center">
     <img src="img/tv.png" height="200px" alt="logo Telaflix">
-    <h1 class="my-1">TELAFLIX 2</h1>
+    <h1 class="my-1">TELAFLIX</h1>
     <form method="POST" action="buscar.php" class="d-flex flex-column mb-3">
       <label for="filme" class="form-label my-2">Insira o nome em <b>INGLÊS</b> do filme ou da série:</label>
       <input type="text" class="form-control-lg" id="filme" name="filme" placeholder="Ex: Top Gun" required>
